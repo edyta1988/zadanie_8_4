91ddaddd01c2268dc5e74df22c4977f39ffbf5ca
Edyta Zawada <edyta.zawada88@gmail.com>
2017-08-06 08:54:47 +0200

    first version of project

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+//coment nr 1
+var name = prompt('Enter your name');
+// linear comment befor "alert line"
+alert('Hello, ' + name);
+/*
+  block comment
+  just in cas it is needed
+*/
+console.log('Hello, ' + name);
